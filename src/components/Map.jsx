4f6c94ad0c68b919ce5de404e6bfa823e07adf12
praveen <praveensofficial1@gmail.com>
2024-10-18 18:09:43 +0530

    updated map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Map = () => {
   return (
     <div className="flex justify-center py-10">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345099466!2d144.95373531584706!3d-37.81720997975155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f84c31d%3A0x5045675218ceed31!2sMelbourne%20Central%2C%20100%20%2F%20King%20St%2C%20Melbourne%20VIC%2C%20Australia!5e0!3m2!1sen!2sus!4v1622200951726!5m2!1sen!2sus"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7773.341893453018!2d77.56725279357912!3d13.056604900000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae194e732fe773%3A0xf9f083e00a4a8893!2sMEDSCRAFT%20HEALTHCARE!5e0!3m2!1sen!2sin!4v1729254919174!5m2!1sen!2sin"
         className="w-full lg:h-[450px] md:h-[400px] h-[300px] border-0"
         allowFullScreen=""
         loading="lazy"
